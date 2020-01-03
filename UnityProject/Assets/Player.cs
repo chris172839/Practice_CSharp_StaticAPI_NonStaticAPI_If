4bebb46e9833351f78ef20974009de1256f4558f
chris172839 <57192859+chris172839@users.noreply.github.com>
2020-01-03 22:45:25 +0800

    HW

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 宣告
+    [Header("血量")]
+    public float Hp = 100;
+    [Header("攻擊力")]
+    public float Atk = 0;
+    [Header("殭屍")]
+    public Zombie zb;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+    #endregion
+
+    public void Attack()
+    {
+        Atk = Random.Range(30f, 36f);
+        print("<color=darkblue>玩家受到傷害: </color>" + Atk);
+    }
+
+    public void Hurt()
+    {
+        Hp -= Atk;
+        print("<color=darkblue>玩家剩下血量: </color>" + Hp);
+
+    }
+
+    public void Dead()
+    {
+        print("<color=darkblue>玩家死亡</color>");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("b"))
+        {
+            Attack();
+            Hurt();
+            if (Hp <= 0)
+            {
+                Dead();
+            }
+        }
+
+    }
+
+}
---
+++ UnityProject/Assets/Zombie.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    #region 宣告
+    [Header("血量")]
+    public float Hp = 100;
+    [Header("攻擊力")]
+    public float Atk = 0;
+    [Header("玩家")]
+    public Player player;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+    #endregion
+
+    public void Attack()
+    {
+        Atk = Random.Range(30f, 40f);
+        print("<color=red>殭屍受到傷害: </color>" + Atk);
+    }
+
+    public void Hurt()
+    {
+        Hp -= Atk;
+        print("<color=red>殭屍剩下血量: </color>" + Hp);
+
+    }
+
+    public void Dead()
+    {
+        print("<color=red>殭屍死亡</color>");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+            Attack();
+            Hurt();
+            if (Hp <= 0)
+            {
+                Dead();
+            }
+        }
+    }
+}
